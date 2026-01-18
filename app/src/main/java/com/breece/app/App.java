4306fa9c6476c578361074a95bce39cd4cfdec6d
manos <emmanouil.psanis@gmail.com>
2026-01-18 21:59:12 +0100

    Refactor project configurations and dependencies: update `pom.xml` versions and plugins, clean up `docker-compose.yml`, remove obsolete `.run` configs, and fix incorrect Spring Boot import.

--- app/src/main/java/com/breece/app/App.java
+++ app/src/main/java/com/breece/app/App.java
@@ -3,7 +3,7 @@ package com.breece.app;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
