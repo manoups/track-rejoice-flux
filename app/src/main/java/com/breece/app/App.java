2bf8ca0261aa82f03cec5c054960b39c74e1853b
manos <emmanouil.psanis@gmail.com>
2026-01-22 09:11:47 +0100

    Enable `@ComponentScan` for `com.breece` package and add HTTP test file for `content` endpoints.

--- app/src/main/java/com/breece/app/App.java
+++ app/src/main/java/com/breece/app/App.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.breece")
 // Explicitly scan for JPA entities (@Entity) in all modules
 @EntityScan(basePackages = "com.breece")
+@ComponentScan(basePackages = {"com.breece"})
 public class App 
 {
     public static void main( String[] args )
