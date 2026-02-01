06741d1ce7a0b0c38018ff073f0d6b1e82640073
manos <emmanouil.psanis@gmail.com>
2026-02-01 16:41:20 +0100

    Remove `@RequiresUser` annotation from `ClaimSighting` as it is no longer necessary.

--- proposal/src/main/java/com/breece/proposal/api/ClaimSighting.java
+++ proposal/src/main/java/com/breece/proposal/api/ClaimSighting.java
@@ -1,6 +1,5 @@
 package com.breece.proposal.api;
 
-
 import com.breece.content.api.model.ContentId;
 import com.breece.coreapi.authentication.Sender;
 import com.breece.coreapi.common.SightingDetails;
@@ -9,13 +8,11 @@ import com.breece.proposal.api.model.LinkedSightingCommand;
 import com.breece.proposal.api.model.LinkedSightingId;
 import com.breece.sighting.api.model.SightingId;
 import io.fluxzero.sdk.persisting.eventsourcing.InterceptApply;
-import io.fluxzero.sdk.tracking.handling.authentication.RequiresUser;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Objects;
 
-@RequiresUser
 public record ClaimSighting(@NotNull ContentId contentId, @NotNull SightingId sightingId, @NotNull SightingDetails sightingDetails, boolean removeAfterMatching, @NotNull LinkedSightingId linkedSightingId) implements LinkedSightingCommand, SightingContentBridge {
 
     @InterceptApply
