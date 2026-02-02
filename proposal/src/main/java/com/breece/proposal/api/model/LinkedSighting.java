2945ed2289e64565f583d01b21ceee4161dd7724
manos <emmanouil.psanis@gmail.com>
2026-02-02 21:28:30 +0100

    Remove `SightingContentBridge` interface and replace `sightingId` with `sourceSightingId` in `ClaimSighting` and related tests. Clean up commented test logic in `ProposalAsyncTest`.

--- proposal/src/main/java/com/breece/proposal/api/model/LinkedSighting.java
+++ proposal/src/main/java/com/breece/proposal/api/model/LinkedSighting.java
@@ -7,10 +7,10 @@ import com.breece.coreapi.user.api.UserId;
 import com.breece.sighting.api.model.SightingId;
 import io.fluxzero.sdk.modeling.Aggregate;
 import io.fluxzero.sdk.modeling.EntityId;
+import io.fluxzero.sdk.modeling.EventPublicationStrategy;
 import jakarta.validation.constraints.NotNull;
-import lombok.With;
 
-@Aggregate(searchable = true)
+@Aggregate(searchable = true, publicationStrategy = EventPublicationStrategy.PUBLISH_ONLY)
 public record LinkedSighting(@EntityId LinkedSightingId linkedSightingId, @NotNull UserId finder,
                              @NotNull UserId seeker, @NotNull SightingId sightingId, @NotNull ContentId contentId,
                              @NotNull SightingDetails sightingDetails) {
