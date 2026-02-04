6493ee6589b307f9730bc5b311fe16046b6ed367
manos <emmanouil.psanis@gmail.com>
2026-02-04 18:53:19 +0100

    Update `SightingErrors#unauthorized` to use `UnauthorizedException` instead of `IllegalCommandException`.

--- sighting/src/main/java/com/breece/sighting/api/SightingErrors.java
+++ sighting/src/main/java/com/breece/sighting/api/SightingErrors.java
@@ -2,12 +2,13 @@ package com.breece.sighting.api;
 
 import io.fluxzero.sdk.common.exception.FunctionalException;
 import io.fluxzero.sdk.tracking.handling.IllegalCommandException;
+import io.fluxzero.sdk.tracking.handling.authentication.UnauthorizedException;
 
 public interface SightingErrors {
     FunctionalException
             alreadyExists = new IllegalCommandException("Sighting already exists"),
             notFound = new IllegalCommandException("Sighting not found"),
             notOwner = new IllegalCommandException("Not source of sighting"),
-            unauthorized = new IllegalCommandException("Unauthorized action"),
+            unauthorized = new UnauthorizedException("Unauthorized action"),
             sightingMismatch = new IllegalCommandException("Sighting details do not match");
 }
