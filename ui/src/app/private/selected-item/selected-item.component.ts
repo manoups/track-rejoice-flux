9b6b05018bf1080e842561f0aef9b2b3d2667405
manos <emmanouil.psanis@gmail.com>
2026-02-14 12:21:11 +0100

    Remove unused `MatButton` import from `SelectedItemComponent` to clean up dependencies.

--- ui/src/app/private/selected-item/selected-item.component.ts
+++ ui/src/app/private/selected-item/selected-item.component.ts
@@ -1,7 +1,7 @@
 import {Component, input} from '@angular/core';
 import {SightingDocument} from '@trackrejoice/typescriptmodels';
 import {MatDrawer, MatDrawerContainer, MatDrawerContent} from '@angular/material/sidenav';
-import {MatButton, MatFabButton} from '@angular/material/button';
+import {MatFabButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 
 @Component({
@@ -10,7 +10,6 @@ import {MatIcon} from '@angular/material/icon';
     MatDrawerContainer,
     MatDrawer,
     MatDrawerContent,
-    MatButton,
     MatFabButton,
     MatIcon
   ],
