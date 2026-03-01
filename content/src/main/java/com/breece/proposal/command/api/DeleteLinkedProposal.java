686931f310336f0c845f5d1dd9f7f14f7982df59
manos <emmanouil.psanis@gmail.com>
2026-03-01 16:06:27 +0100

    remove weighted association on content deletion

--- content/src/main/java/com/breece/proposal/command/api/DeleteLinkedProposal.java
+++ content/src/main/java/com/breece/proposal/command/api/DeleteLinkedProposal.java
@@ -3,14 +3,10 @@ package com.breece.proposal.command.api;
 import com.breece.content.api.model.ContentId;
 import com.breece.proposal.command.api.model.WeightedAssociationId;
 import com.breece.proposal.command.api.model.WeightedAssociationUpdate;
+import io.fluxzero.sdk.modeling.EventPublicationStrategy;
+import io.fluxzero.sdk.persisting.eventsourcing.Apply;
 
 public record DeleteLinkedProposal(ContentId contentId, WeightedAssociationId weightedAssociationId) implements WeightedAssociationUpdate {
-
-/*    @AssertLegal
-    void assertPermitted(WeightedAssociation weightedAssociation, Sender sender) {
-        if (sender.nonAuthorizedFor(weightedAssociation.finder())) {
-            throw WeightedProposalErrors.unauthorized;
-        }
-    }*/
-
+    @Apply(publicationStrategy = EventPublicationStrategy.PUBLISH_ONLY)
+    void apply() {}
 }
