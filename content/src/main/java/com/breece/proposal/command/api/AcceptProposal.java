7f1863005dc104edcd9718529d775da606979ca8
manos <emmanouil.psanis@gmail.com>
2026-03-01 15:12:20 +0100

    make actions on state publish only

--- content/src/main/java/com/breece/proposal/command/api/AcceptProposal.java
+++ content/src/main/java/com/breece/proposal/command/api/AcceptProposal.java
@@ -3,7 +3,11 @@ package com.breece.proposal.command.api;
 import com.breece.content.api.model.ContentId;
 import com.breece.proposal.command.api.model.WeightedAssociationId;
 import com.breece.proposal.command.api.model.WeightedAssociationUpdateOwner;
+import io.fluxzero.sdk.modeling.EventPublicationStrategy;
+import io.fluxzero.sdk.persisting.eventsourcing.Apply;
 
 public record AcceptProposal(ContentId contentId,
                              WeightedAssociationId weightedAssociationId) implements WeightedAssociationUpdateOwner {
+    @Apply(publicationStrategy = EventPublicationStrategy.PUBLISH_ONLY)
+    void apply() {}
 }
--- content/src/main/java/com/breece/proposal/command/api/RejectProposal.java
+++ content/src/main/java/com/breece/proposal/command/api/RejectProposal.java
@@ -3,6 +3,10 @@ package com.breece.proposal.command.api;
 import com.breece.content.api.model.ContentId;
 import com.breece.proposal.command.api.model.WeightedAssociationUpdateOwner;
 import com.breece.proposal.command.api.model.WeightedAssociationId;
+import io.fluxzero.sdk.modeling.EventPublicationStrategy;
+import io.fluxzero.sdk.persisting.eventsourcing.Apply;
 
 public record RejectProposal(ContentId contentId, WeightedAssociationId weightedAssociationId) implements WeightedAssociationUpdateOwner {
+    @Apply(publicationStrategy = EventPublicationStrategy.PUBLISH_ONLY)
+    void apply() {}
 }
